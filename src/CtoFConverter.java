51218aac895bd77b2651ee4369c3a0420b48d32f
sachet-abey <155912536+sachet-abey@users.noreply.github.com>
2024-02-18 13:41:31 -0500

    Update CtoFConverter.java

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner; // Import Scanner from Java
+public class CtoFConverter {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scan = new Scanner(System.in); // Create a scanner to read user input
+        double celsius; // Define celsius variable that will be initialized from user input
+        String trash; // Define trash variable for any invalid user input
+        boolean done = false; // Initialize done variable for the do-while loop. This is set to true when the user enters a valid input
+        do {
+            System.out.println("Enter a temperature in celsius: "); // Prompts user to enter temperature in celsius
+            if(scan.hasNextDouble()) { // If the next input can be read as a double
+                celsius = scan.nextDouble(); // Read celsius value because valid input
+                double farenheit = 1.8 * celsius + 32; // Calculate farenheit value by multiplying the celsius value by 9/5 (1.8) and then adding 32
+                System.out.println("The temperature in farenheit is "+farenheit); // Output the farenheit calculation/result
+                scan.nextLine(); // Clears the newline from the buffer
+                done = true; // Set done to true because we are finished - no more looping needed because the user entered a valid input
+            }
+            else {
+                trash = scan.nextLine(); // Read the input as a String because input cannot be read as a double
+                System.out.println("You entered "+trash+". Please enter a valid temperature value!"); // Outputs the invalid input and asks user to enter a valid temperature value
+            }
         }
+        while (!done); // Keeps looping when done is false (done becomes true when user enters a valid input)
     }
 }
